b0930924eb41445bbca451b884e11436bad7e21f
unknown <you@example.com>
2026-02-25 20:21:53 -0300

    controller criado

--- school/src/main/java/com/evm/school/services/AlunoService.java
+++ school/src/main/java/com/evm/school/services/AlunoService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AlunosService {
+public class AlunoService {
 
     @Autowired
     private AlunoRepository alunoRepository;
